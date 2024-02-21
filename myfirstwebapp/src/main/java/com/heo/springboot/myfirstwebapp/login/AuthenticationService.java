6ad3e6a4ed967ab3193e0b6ee4df4963ebdc8975
Kiyoung Heo <rldud035@naver.com>
2024-02-21 16:17:46 +0900

    Todo web appliction first

---
+++ myfirstwebapp/src/main/java/com/heo/springboot/myfirstwebapp/login/AuthenticationService.java
@@ -0,0 +1,14 @@
+package com.heo.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+	public boolean authenticate(String username, String password) {
+		boolean isValidUsername = username.equalsIgnoreCase("Kim");
+		boolean isValidPassword = password.equalsIgnoreCase("dummy");
+
+		return isValidUsername && isValidPassword;
+	}
+}
---
+++ myfirstwebapp/src/main/java/com/heo/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,49 @@
+package com.heo.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+	private static List<Todo> todos = new ArrayList<>();
+	private static int sequence = 0;
+	static {
+		todos.add(new Todo(++sequence, "Kim", "Learn Aws", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++sequence, "Kim", "Learn devOps", LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(++sequence, "Kim", "Learn Full Stack", LocalDate.now().plusYears(3), false));
+	}
+
+	public List<Todo> findByUsername(String username) {
+
+		// 로직 작성 필요
+
+		return todos;
+	}
+
+	public void addTodo(Todo todo) {
+		todo.setId(++sequence);
+		todos.add(todo);
+	}
+
+	public void deleteTodo(int id) {
+//		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(todo -> todo.getId() == id);
+//		todos.removeIf(predicate);
+	}
+
+	public Todo findById(int id) {
+		return todos.stream()
+				.filter(todo -> todo.getId() == id)
+				.findAny().get();
+	}
+
+	public void updateTodo(Todo todo) {
+		deleteTodo(todo.getId());
+		todos.add(todo);
+	}
+
+}
